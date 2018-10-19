f0bc12e43df4acac47878de347dd070254a65777
mgmdi <14-10444@usb.ve>
2018-10-19 10:39:50 -0400

    Agregue pruena 1 caso frontera
    Se agrego caso de prueba frontera: se prueba que el usuario pueda registrarse
    satisfactoriamente con un formato de contrasena con
    - Exactamente 3 letras
    - Exactamente una letra mayuscula
    - numero de digitos indeterminado
    - longitud de 8 caracteres

---
+++ usuarios/test_seguridad.py
@@ -0,0 +1,42 @@
+# encoding=utf-8
+from django.test import TestCase
+
+# Se importa el modulo de PyUnit para realizar las
+# pruebas unitarias
+import unittest
+from seguridad import Seguridad
+
+# AGREGANDO CASOS DE PRUEBA: FRONTERA, MALICIA Y MALICIA
+
+class TestSeguridad(unittest.TestCase):
+
+    def setUp(self):
+		self.seguridad = Seguridad()
+
+	def tearDown(self):
+		self.seguridad = None
+
+    
+    '''
+    PRUEBA 1: Formato de contrasena
+    Tipo: Frontera
+    Funcion a probar: registrarUsuario en seguridad.py
+    Descripcion del caso: se pone a prueba que se registre el usuario de forma correcta con una contrasena
+    con:
+    Caracteristicas de contrasena:
+        1) Exactamente 3 letras
+        2) Exactamente una letra mayuscula
+        3) Nuemero de digitos indeterminado 
+        4) Longitud de 8 caracteres
+    Caracteristicas de email:
+        1) email cumple con RFC 822
+
+    Resultado de la prueba: el usuario se registra satisfactoriamente
+    '''
+    def test_frontera_registrarUsuarioP1(self):
+        self.assertTrue(self.seguridad.registrarUsuario("mjgf@gmail.com", "Maa15678","Maa15678"))
+        self.assertTrue(self.seguridad.registrarUsuario("andres@gmail.com", "1And5200","1And5200"))
+        self.assertTrue(self.seguridad.registrarUsuario("mariafeix@gmail.com", "0Myf5430","0Myf5430"))
+        self.assertTrue(self.seguridad.registrarUsuario("angProco@gmail.com", "Ang00000","Ang00000"))
+        self.assertTrue(self.seguridad.registrarUsuario("josegp@gmail.com", "11Pmj111","11Pmj111"))
+        
