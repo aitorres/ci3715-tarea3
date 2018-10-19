6c51e052a446cf8675153767be7b5b3e27f32622
Jawil Ricauter <ricauterjawil@gmail.com>
2018-10-18 22:32:44 -0400

    TDD, Prueba 4. Se agrega una funcion para verificar la longitud de una clave, y otra auxiliar par a ejecutar todas las validaciones a las claves requeridas. Ahora la prueba pasa.

--- usuarios/seguridad.py
+++ usuarios/seguridad.py
@@ -7,10 +7,15 @@ y la autenticacion de los usuarios siguiendo algunas reglas de seguridad.
 class Seguridad:
 
 	def __init__(self):
-		pass
+		# NOTA: Se asume que el rango de claves es inclusivo,
+		# es decir, que es valido tener 8 caracteres y que
+		# es valido tener 16 caracteres
+
+		self.longitud_minima = 8
+		self.longitud_maxima = 16
 
 	def registrarUsuario(self, email, clave1, clave2):
-		return self.es_correo_valido(email) and self.claves_coinciden(clave1, clave2)
+		return self.es_correo_valido(email) and self.es_clave_valida(clave1, clave2)
 
 	def es_correo_valido(self, email):
 		# El regex a continuacion interpreta el formato de correos
@@ -21,5 +26,18 @@ class Seguridad:
 			return True
 		return False
 
+	def es_clave_valida(self, clave1, clave2):
+		if not self.claves_coinciden(clave1, clave2):
+			return False
+
+		if not (self.clave_tiene_longitud_correcta(clave1) and self.clave_tiene_longitud_correcta(clave2)):
+			return False
+
+		return True 
+
 	def claves_coinciden(self, clave1, clave2):
 		return clave1 == clave2
+
+	def clave_tiene_longitud_correcta(self, clave):
+		tamano_clave = len(clave)
+		return (self.longitud_minima <= tamano_clave) and (tamano_clave <= self.longitud_maxima)
